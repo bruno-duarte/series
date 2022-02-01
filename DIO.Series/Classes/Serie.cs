72f915aeb41a6a7c7066170d5575b7626e18ffc6
Bruno Duarte <bruno.duarte@aluno.uece.br>
2022-02-01 19:34:06 -0300

    Primeiro commit

---
+++ DIO.Series/Classes/Serie.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DIO.Series
+{
+    public class Serie : EntidadeBase
+    {
+        // Atributos
+        private Genero Genero { get; set; }
+        private string Titulo { get; set; }
+        private string Descricao { get; set; }
+        private int Ano { get; set; }
+        private bool Excluido { get; set; }
+
+        public Serie(int id, Genero genero, string titulo, string descricao, int ano)
+        {
+            Id = id;
+            Genero = genero;
+            Titulo = titulo;
+            Descricao = descricao;
+            Ano = ano;
+            Excluido = false;
+        }
+
+        public override string ToString()
+        {
+            // Enviroment.NewLine https://docs.microsoft.com/en-us/dotnet/api/system.environment.newline?view=netcore-3.1
+            string retorno = "";
+            retorno += "Genero: " + this.Genero + Environment.NewLine;
+            retorno += "Titulo: " + this.Titulo + Environment.NewLine;
+            retorno += "Descrição: " + this.Descricao + Environment.NewLine;
+            retorno += "Ano de Início: " + this.Ano + Environment.NewLine;
+            retorno += "Excluído: " + (this.Excluido ? "Sim" : "Não");
+
+            return retorno;
+        }
+
+        public string retornaTitulo()
+        {
+            return this.Titulo;
+        }
+
+        public int retornaId()
+        {
+            return this.Id;
+        }
+
+        public bool retornaExcluído()
+        {
+            return this.Excluido;
+        }
+
+        public void Excluir()
+        {
+            this.Excluido = true;
+        }
+    }
+}
